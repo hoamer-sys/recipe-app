1349e2be6afe602587717ed752754fd7b845ab72
Michael Troncelliti <mike@MacBook-Air-2.local>
2026-03-24 22:13:02 -0400

    more recipe not found tinkering

--- app/recipes/[id]/page.tsx
+++ app/recipes/[id]/page.tsx
@@ -6,7 +6,7 @@ export default async function RecipePage({ params }: { params: { id: string } })
   const { data: recipe, error } = await supabase
     .from("recipes")
     .select("*")
-    .eq("id", Number(params.id))
+    .eq("id", params.id)
     .single();
 
   if (error || !recipe) {
